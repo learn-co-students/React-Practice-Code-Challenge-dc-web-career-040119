76d3f6a48f224f24f94b84e3faae35d6da46c438
github username <github email address>
2019-06-11 17:16:41 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,81 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import Button from './components/MoreButton'
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+
+    this.state = {
+      allSushis: [],
+      currentPageIndex: 0,
+      idOfSushisEaten: [],
+      moneyAvailable: 100
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushis => {
+      this.setState({
+        allSushis: sushis
+      })
+    })
+  }
+
+  deriveSushiGivenStartingIndex = () => {
+    let startingIndex = this.state.currentPageIndex
+    //slice is used because we need 4 sushi at a time. we are making a copy of the allSushis and returning a new array with 4 sushis.
+    //using slice which doesn't include the end index, so if we just do startingIndex + 3 we will be one short
+    let lastIndex = startingIndex + 4
+
+    return this.state.allSushis.slice(startingIndex, lastIndex)
+  }
+
+  handleMoreButton = () => {
+    let newIndex = this.state.currentPageIndex + 4
+
+    this.setState({
+      currentPageIndex: newIndex
+    })
+  }
+
+  eatSushiFunc = (id, price) => {
+    //this is where the moneyAvailable is used
+    let moneyRemaining = this.state.moneyAvailable - price
+
+    if (moneyRemaining < 0){
+      alert('Not enough money!')
+      return
+    }
+    // use spread operator to make a copy of original array and pass in sushiObj
+    let copy = [...this.state.idOfSushisEaten]
+
+    copy.push(id)
+
+    // update the state of idOfSushisEaten to the copy of that ID which contains the ID of what just got eaten
+    this.setState({
+      idOfSushisEaten: copy,
+      moneyAvailable: moneyRemaining
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushi={this.deriveSushiGivenStartingIndex()} eatSushiFunc={this.eatSushiFunc} idOfSushisEaten={this.state.idOfSushisEaten}
+          handleMoreButton={this.handleMoreButton}
+          />
+        <Table
+          idOfSushisEaten={this.state.idOfSushisEaten}
+          moneyAvailable={this.state.moneyAvailable}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+//a functional component passes in props as an argument so I can just call props directly. we only use this.props within a class since we are referring to an instance of props
+// debugger
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+
+          {
+            props.sushi.map((sushiObj) => {
+              return <Sushi sushiObj={sushiObj} key={sushiObj.id} idOfSushisEaten={props.idOfSushisEaten} eatSushiFunc={props.eatSushiFunc}
+              moneyRemaining={props.moneyRemaining}/>
+            })
+          }
+
+        <MoreButton handleMoreButton={props.handleMoreButton}/>
       </div>
     </Fragment>
   )
