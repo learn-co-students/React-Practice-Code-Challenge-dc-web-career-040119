ce28225efd8d3a3843897a2179a0e6f14aacdf52
Justin <justinhwu95@gmail.com>
2019-06-12 16:48:25 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,64 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      allSushi: [],
+      eatenSushiIDs: [],
+      startingIndex: 0,
+      money: 100
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp=>resp.json())
+    .then(sushiObj=>{
+      this.setState({
+        allSushi: sushiObj
+      })
+    })
+  }
+
+  eatSushiHandler = (id, price) => {
+    let total = this.state.money - price
+    if(!this.state.eatenSushiIDs.includes(id) && total>=0){
+      this.setState({
+        eatenSushiIDs: [...this.state.eatenSushiIDs, id],
+        money: total
+    })
+    }
+  }
+
+  deriveSushiGivenCurrentPage = () => {
+    let startingIndex = this.state.startingIndex
+    let lastIndex = startingIndex + 4
+    return this.state.allSushi.slice( startingIndex, lastIndex )
+  }
+
+  moreButtonHandler = () => {
+    if(this.state.startingIndex <= this.state.allSushi.length){
+    this.setState({
+      startingIndex: (this.state.startingIndex+4)
+    })
+    }
+    else{
+      this.setState({
+        startingIndex: 0
+      })
+
+    }
+  }
+
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          eatenSushiIDs={this.state.eatenSushiIDs} sushiToDisplay={this.deriveSushiGivenCurrentPage()} eatSushiHandler={this.eatSushiHandler}
+          moreButtonHandler={this.moreButtonHandler}/>
+        <Table eatenSushiIDs={this.state.eatenSushiIDs} calculateTotal={this.state.money}/>
       </div>
     );
   }
